70753272dd8c654aabd003135c6330a62ecc0cc7
carlos-paezf <cpaezferreira@gmail.com>
2023-01-17 22:15:21 -0500

    Update Config

--- Backend/src/config/data.config.ts
+++ Backend/src/config/data.config.ts
@@ -1,6 +1,7 @@
 import { DataSource, DataSourceOptions } from "typeorm"
 import * as dotenv from 'dotenv'
 import { SnakeNamingStrategy } from 'typeorm-naming-strategies'
+import path from "path"
 
 
 /* Loading the environment variables from the .env file. */
@@ -19,8 +20,8 @@ const Config: DataSourceOptions = {
     database: process.env.DB_DATABASE,
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    entities: [ __dirname + "/../models/*.entity(.ts,.js)" ],
-    migrations: [ __dirname + "/../migrations/*(.ts,.js)" ],
+    entities: [ path.join( __dirname, `/../entities/*.entity{.ts,.js}` ) ],
+    migrations: [ path.join( __dirname, `/../migrations/*{.ts,.js}` ) ],
     synchronize: false,
     migrationsRun: true,
     logging: false,
